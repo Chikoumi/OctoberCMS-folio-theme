f5d2a08f959018658ee27dae76042ea579423962
Chikoumi <chk@harmony-hosting.com>
2015-05-30 18:15:04 +0200

    First Commit
    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/* One page System Run ************************************************
+Launch One page System on document load ******************************/
+$(document).ready(function() {
+    $(function() { $.scrollIt(); });
+});
+/* END One page System Run ******************************************/
